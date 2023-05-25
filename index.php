aa0f22dc7655c88ae39d97547880578ee5e2631d
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-05-25 11:01:13 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+phpinfo();
+// require_once('./db_connection.php');
+
+// header("Content-Type:application/json");
+// if ($_SERVER["REQUEST_METHOD"] == "GET") {
+//     require('./get_todos.php');
+//     get_all_todos();
+// }
+
+?>
